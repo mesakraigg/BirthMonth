ad5a400ac835b499d5afaf2492e391680352fb8e
mesak <mrai3826@zenithacademy.org>
2026-03-03 14:51:43 -0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+
+        Scanner imput = new Scanner(System.in);
+
+        int month;
+
+        System.out.print("Enter your birth month (1-12): ");
+        month = imput.nextInt();
+
+        if (month >= 1 && month <= 12)
+        {
+            System.out.println("your birth month is: " + month);
+        }
+        else
+        {
+            System.out.println("You entered an incorrect month value: " + month);
+        }}}
